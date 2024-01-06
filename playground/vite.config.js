f5c3b19a281ee973e468ccd91d656e41ce867811
haixin-fang <1875934025@qq.com>
2024-01-06 14:33:43 +0800

    ci: 打包错误修复

--- playground/vite.config.js
+++ playground/vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from "vite";
 import path from "path";
 import vue from "@vitejs/plugin-vue";
 const alias = [];
-console.log(process.env.NODE_ENV)
+console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV != "production") {
   alias.push({ find: /^tojson.js$/, replacement: path.join(__dirname, "../packages/tojson.js/src/index.js") });
   alias.push({ find: /^sketch$/, replacement: path.join(__dirname, "../packages/sketchtojson/src/index.js") });
+} else {
+  alias.push({ find: /^tojson.js$/, replacement: path.join(__dirname, "node_modules", "tojson.js") });
 }
 export default defineConfig({
   resolve: {
