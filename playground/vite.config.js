ea97b2451539a932385aa5aa576b6d3668694ef6
haixin-fang <1875934025@qq.com>
2024-01-06 14:10:21 +0800

    ci: 更新

--- playground/vite.config.js
+++ playground/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import path from "path";
 import vue from "@vitejs/plugin-vue";
 const alias = [];
+console.log(process.env.NODE_ENV)
 if (process.env.NODE_ENV != "production") {
   alias.push({ find: /^tojson.js$/, replacement: path.join(__dirname, "../packages/tojson.js/src/index.js") });
   alias.push({ find: /^sketch$/, replacement: path.join(__dirname, "../packages/sketchtojson/src/index.js") });
